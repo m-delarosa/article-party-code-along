267d5ce5a4d98274941723c5dd8017bf4a209e8e
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-03-15 23:51:55 -0600

    Add Crud to all classes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :articles, only: [:index, :show, :create, :update, :destroy]
   resources :authors, only: [:index, :show, :create, :update, :destroy]
   resources :magazines, only: [:index, :show, :create, :update, :destroy]
+  post "/post/magazines", to: "magazines#create"
   #resources :authors, only: [:index, :show, :create, :update, :destroy]
 
   #you can chain actions in the format above. Don't want to do this
@@ -9,10 +10,7 @@ Rails.application.routes.draw do
   
   #get "/authors", to: "authors:index"
   #get "/authors/:id", to: "author#show"
-
   #when your controller names don't match up perfectly, you need to use
   #this method instead, but in this case it's the same as using "resources"
   #because the controller name and models match up. 
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,14 @@ Magazine.destroy_all
 
 a1 = Author.create( name: "Edgar Allen Poe", age: 45 )
 a2 = Author.create( name: "Robert Frost", age: 45 )
+a3 = Author.create( name: "Hunter S. Thompson", age: 61)
 
 m1 = Magazine.create( name: "Sports Illustrated", category: "Sports")
 m2 = Magazine.create( name: "Max PC", category: "Technology")
+m3 = Magazine.create( name: "Rolling Stone", category: "Lifestyle")
 
 Article.create(title: "How to Code", author: a1, magazine: m2 )
 Article.create(title: "League Review", author: a2, magazine: m1 )
-Article.create(title: "Pyhthon Illustrated", author: a2, magazine: m1 )
+Article.create(title: "Python Illustrated", author: a2, magazine: m2 )
+Article.create(title: "Fear and Loathing in Las Vegas", author: a3, magazine: m3 )
+Article.create(title: "Can you delete me", author: a3, magazine: m3 )
